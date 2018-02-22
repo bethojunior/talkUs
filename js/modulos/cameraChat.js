d8e2d17d87b75ce8758e0bf207c42305cffc9143
betho <hgomescostajr@gmail.com>
2018-02-22 16:02:48 -0300

    apanhando ainda para enviar imagem em chat direto da camera

--- js/modulos/cameraChat.js
+++ js/modulos/cameraChat.js
@@ -12,19 +12,19 @@ var app = {
     capturePhoto : function(){
         $('.fixed-action-btn').closeFAB();
         navigator.camera.getPicture(onSuccess, onFail, { quality: 100,
-            destinationType: Camera.DestinationType.FILE_URI });
-        function onSuccess(imageURI) {
-            
+            destinationType: Camera.DestinationType.FILE_URI,
+            saveToPhotoAlbum: true
+        });
+        function onSuccess(imageURI){
             $('#sendFilePhoto').modal('open');
             var image  = document.getElementById("myImage");
             image.src = imageURI;
-            let src = document.getElementById("srcPhoto").value(imageURI);
+            let src = document.getElementById("srcPhoto").value;
+            src.file = "data:image/jpeg;base64," + imageURI;
         }
-
         function onFail(message) {
             alert('Failed because: ' + "Error");
         }
-
         function clearCache(){
             navigator.camera.cleanup();
         }
